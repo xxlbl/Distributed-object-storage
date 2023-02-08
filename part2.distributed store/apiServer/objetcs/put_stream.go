27c9f24dc270e2d8f0e0da3300009de82ef28e72
ljf <937038264@qq.com>
2023-02-08 19:59:54 +0800

    补充

--- part2.distributed store/apiServer/objetcs/put_stream.go
+++ part2.distributed store/apiServer/objetcs/put_stream.go
@@ -7,6 +7,7 @@ import (
 	"../heartbeat"
 )
 
+//objeetstream 包是对 Go 语言 http 包的一个封装，用来把一些 http 函数的调用转换成读写流的形式方便处理
 func putStream(object string) (*objectstream.PutStream, error) {
 	server := heartbeat.ChooseRandomDataServer()
 	if server == "" {
